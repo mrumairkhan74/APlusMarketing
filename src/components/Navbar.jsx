f6cca27893d71aff31707bef687c3554e08b1db1
Umair Khan <125242896+mrumairkhan74@users.noreply.github.com>
2025-06-24 17:47:32 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
         <NavLink to="/" className="text-xl font-bold text-blue-600 flex items-center gap-2">
           <img
           title='Logo'
-            src="https://res.cloudinary.com/mrumairkhan74/image/upload/v1750613892/A_logo_iw0zej.png"
+            src="https://res.cloudinary.com/mrumairkhan74/image/upload/v1750769146/A_Plus_Logo_wnc4rr.png"
             alt="A+ Logo"
             className="w-36 h-20 object-contain mix-blend-darken drop-shadow-lg"
           />
